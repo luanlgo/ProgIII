2f663480ec41f2aecdaebfce0762115fc498b4f3
Luan Gomes <luangm22@gmail.com>
2019-10-28 22:12:59 -0300

    cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	var isCadastro = true;
+	var isLogin = true;
 
 	var labelButton = $(document).find(".label-cadastro");
 	var contentCadastro = $(document).find(".cadastro");
@@ -8,20 +8,98 @@ $(document).ready(function() {
 
 	var cadastroButton = $(document).find(".button-cadastro");
 
+	const HOME_URL = "file:///C:/Users/Luan%20Gomes/Desktop/Nova%20pasta/ProgIII/home.html";
+
+	var form = $("#form");
+
 	//funcao label de cadastro 
 	labelButton.on("click", function() {
 
-	if (isCadastro) {
-		contentLogin.hide();
-		contentCadastro.show();
-	} else {
-		contentCadastro.hide();
-		contentLogin.show();
+		if (isLogin) {
+			contentLogin.hide();
+			contentCadastro.show();
+		} else {
+			contentCadastro.hide();
+			contentLogin.show();
+		}
+
+	    isLogin = !isLogin
+	    removeRequired();
+	    labelButton.html(isLogin? "Cadastre-se já!": "Voltar");
+	    cadastroButton.val(isLogin? "Acessar": "Cadastrar");
+	});
+
+	form.on("submit", function() {
+
+		var emailCad = $(document).find("#email_cad");
+		var senhaCad = $(document).find("#senha_cad");
+
+		var emailLog = $(document).find("#email_log");
+		var senhaLog = $(document).find("#senha_log");
+
+		if (!isLogin) {
+			cadastrarLocalStorage(emailCad.val(), senhaCad.val());
+		} else {
+			logarLocalStorage(emailLog.val(), senhaLog.val());
+		}
+
+		return false;
+	});
+
+	function logarLocalStorage(email, senha) {
+		var usuario = window.localStorage[email];
+		if(usuario === undefined) {
+			alert("Email ou Senha errados");
+		}
+		else {
+			if (senha === usuario) {
+				window.location=HOME_URL;
+			} else {
+				alert("Senha incorreta")
+			}
+		}
 	}
 
-    isCadastro = !isCadastro
-    labelButton.html(isCadastro? "Cadastre-se já!": "Voltar");
-    cadastroButton.val(isCadastro? "Acessar": "Cadastrar");
-	});
+	function cadastrarLocalStorage(email, senha) {
+		var usuario = getLocalStorage(email);
+		if (usuario === undefined) {
+			alert("Email já cadastrado");
+		} else {
+			if (isValid()) {
+				window.localStorage.setItem(email, senha);
+			}
+		} 
+	};
+
+	function getLocalStorage(key) {
+		if (typeof window.localStorage === undefined){
+			return undefined;
+		}
+		return window.localStorage.getItem(key);
+	}
+
+	function removeRequired() {
+		if (isLogin) {
+			$("#email_log").attr("required", "required");
+			$("#senha_log").attr("required", "required");
+
+			$("#email_cad").removeAttr("required");
+			$("#senha_cad").removeAttr("required");
+		} else {
+			$("#email_cad").attr("required", "required");
+			$("#senha_cad").attr("required", "required");
+
+			$("#email_log").removeAttr("required");
+			$("#senha_log").removeAttr("required");
+		}
+	}
+
+	function isValid() {
+		if (isLogin) {
+			return $("#email_log").val() !== "" && $("#senha_log").val() !== "";
+		} else {
+			return $("#email_cad").val() !== "" && $("#senha_cad").val() !== "";
+		}
+	}
 
 });
