cc9cc3f436b381ee089c751a480fdf4baf39345d
Luan Gomes Milano <lmilano@furb.br>
2019-11-04 21:42:00 -0300

    backend

---
+++ backend/src/main/java/controller/UserController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+	
+	/*@RequestMapping("/")
+	public String index() {
+		
+	}*/
+	
+}
